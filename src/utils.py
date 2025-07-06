1ca8b77d50f0cd44cbf38ce7552ba866276b9171
harikesh-777 <harikeshreji@gmail.com>
2025-07-07 00:59:45 +0530

    Flask Backend initialised

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,13 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
 def evaluate_models(X_train,y_train,x_test,y_test,models,params):
     try:
         report={}
@@ -28,7 +35,7 @@ def evaluate_models(X_train,y_train,x_test,y_test,models,params):
             gs = GridSearchCV(model,param,cv=3)
             gs.fit(X_train,y_train)
             model = gs.best_estimator_
-            
+
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(x_test)
 
